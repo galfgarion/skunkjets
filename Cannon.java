8dafcaa0f1134b62d988c006334838a0fcaf2002
Evan Ovadia <Verdagon@pcp056625pcs.wireless.calpoly.edu>
2010-01-30 22:36:43 -0800

    fixed firing in the start

--- Cannon.java
+++ Cannon.java
@@ -1,6 +1,5 @@
 import java.util.ArrayList;
 
-import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.util.vector.Vector2f;
 
@@ -12,7 +11,7 @@ class Cannon extends GameObject
 	private float radius;
 	private float red, green, blue;
 
-	private double lastFireTime = 0;
+	private double lastFireTime = -10000;
 
 	private ProjectileType curProjectile = new RocketProjectile();
 	ArrayList<Bullet> bullets;
@@ -28,16 +27,16 @@ class Cannon extends GameObject
 		bullets = new ArrayList<Bullet>();
 	}
 
-	public Cannon setColor(float red, float green, float blue)
-	{
+	public Cannon setColor(float red, float green, float blue) {
 		this.red = red;
 		this.green = green;
 		this.blue = blue;
 		return this;
 	}
 
-	public void update(double timeDelta)
-	{
+	@Override
+	public void update(SkunkJets.FrameData frameData) {
+		super.update(frameData);
 	}
 
 	public double getOrientation()
@@ -50,15 +49,22 @@ class Cannon extends GameObject
 		this.orientation = orientation;
 	}
 
-	public boolean canFire(double time)
+	public boolean canFire()
 	{
-		return ((time - lastFireTime) >= (1.0f / curProjectile.getFiringRate()))
+		System.out.println((SkunkJets.mainTimer.getTime() - lastFireTime) + " " + (1.0f / curProjectile.getFiringRate()));
+		return ((SkunkJets.mainTimer.getTime() - lastFireTime) >= (1.0f / curProjectile.getFiringRate()))
 			&& (curProjectile.count < curProjectile.maxBullets);
 	}
 
-	public Bullet fire(double time)
-	{
-		lastFireTime = time;
+	public Bullet fire() {
+		assert canFire();
+		
+		double oldLastFireTime = lastFireTime;
+		System.out.println("last fire time: " + lastFireTime);		
+		lastFireTime = SkunkJets.mainTimer.getTime();
+		System.out.println("now: " + lastFireTime);
+		System.out.println("difference: " + (lastFireTime - oldLastFireTime));
+		
 		float velX = (float) (curProjectile.getSpeed() * Math.cos(orientation * DEG_TO_RAD));
 		float velY = (float) (curProjectile.getSpeed() * Math.sin(orientation * DEG_TO_RAD));
 
@@ -66,6 +72,7 @@ class Cannon extends GameObject
 		//TODO fix start of bullet
 		Bullet bullet = curProjectile.fire(new Vector2f(getPosition()), velocity);
 		bullets.add(bullet);
+		
 		return bullet;
 	}
 
